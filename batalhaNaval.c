e7628b662ac51d1416149e5bbcac8506122d7264
guilhermeleeno <guilhermelino@msn.com>
2025-07-29 18:57:30 -0300

    batalhaNaval.c
    nivel aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    int num_coluna[10];
-    char colunas[10] = {'A', 'B', 'C','D','E','F', 'G', 'H', 'I','J'}; // as colunas que vão rodar no loop
-    int tabuleiro[10][10] = {0}; // aqui ele faz o zero virar agua
+    //int num_coluna[10]; // se quiser da pra ativar pra melhorar o endereçamento da fita
+    char colunas[10] = {'A', 'B', 'C','D','E','F', 'G', 'H', 'I','J'};
+    int tabuleiro[10][10] = {}; // aqui ele faz o zero virar agua
 
     // Colocando os navios nos endereços, 0 É A - 1 É B E POR AI VAI a parte vertical
-    tabuleiro[1][0] = tabuleiro[2][0] = tabuleiro[3][0] = 3;
+    tabuleiro[4][0] = tabuleiro[5][0] = tabuleiro[6][0] = 3;
 
     // aqui entra os horizontais - deixei zero pra ficar mais facil (se não tem que fazer sempre -1 pra bater)
-    tabuleiro[4][4] = tabuleiro[4][5] = tabuleiro[4][6] = 3;
+    tabuleiro[9][3] = tabuleiro[9][4] = tabuleiro[9][5] = 3;
+
+
+    //inserindo navios na diagonal que tem as coordenadas iguais - esquerda
+
+    for(int i=0; i<3;i++){
+        tabuleiro[i][i]=3; //facil aqui porque é só considerar o seguinte: cada i nesse loop for vai entrar repetido na matriz tabuleiro, representando a diagonal
+
+    }
+
+    //diagonal que vem da direita
+    for(int i=0; i<3; i++){
+        tabuleiro[i][9-i] = 3; //aqui a gente pega qual coluna quer fazer a diagonal e subtrai o numero dela do i (como o navio tem 3 casas o 2C é o limite pra ficar certo, se não ele aparece do outro lado)
+    }
 
     // Exibindo o tabuleiro
     printf("\n\n\t    BATALHA NAVAL   \n\n"); // ajuste pra centralizar o texto
@@ -17,8 +30,8 @@ int main() {
     //
     printf("    "); // aqui o\t não funcionou pra alinhar, tive que digitar 4 espaços... ai deu
     for (int i = 0; i < 10; i++) {
-        //printf("%d", i); // achei legal colocar o numero na frente para saber como identificar o numero da coluna
-        printf("%c    ", colunas[i]); // mais espaços pra encaixar, e as letras da coluna vai entrar no lugar do numero quer seria o i - pra usar o printf de cima colocar +2 espaços aqui
+        printf("%d", i); // achei legal colocar o numero na frente para saber como identificar o numero da coluna
+        printf("%c   ", colunas[i]); // mais espaços pra encaixar, e as letras da coluna vai entrar no lugar do numero quer seria o i
     }
     printf("\n");
 
